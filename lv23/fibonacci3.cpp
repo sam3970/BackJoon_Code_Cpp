8fa710797ea808d883b003ecc2a0e6b25f693030
SangKyu Lee <sam3970@gmail.com>
2020-07-23 21:17:18 +0900

    fibonacci dp baekjoon ver

--- lv23/fibonacci3.cpp
+++ lv23/fibonacci3.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 /* notice : 1,000,000으로 나눈 fibonacci numbers의 반복되는 주기는 1,500,000 */
 
-long long arr[1500001];
+long long arr[pivot];
 int limit = 1000000;
 
 void fib()
@@ -14,7 +14,7 @@ void fib()
 	arr[0] = 0;
 	arr[1] = 1;
 	
-	for (int i = 0; i < 1500000; i++) 
+	for (int i = 0; i < pivot; i++) 
 	{
 		arr[i + 2] = (arr[i + 1] + arr[i]) % limit;
 	}	
