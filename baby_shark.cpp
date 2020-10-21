ee4ec405188e1d660a7d271fb005a6be872bf2d6
SangKyu Lee <sam3970@gmail.com>
2020-10-21 16:03:36 +0900

    baby shark ver5

--- baby_shark.cpp
+++ baby_shark.cpp
@@ -8,34 +8,55 @@ using namespace std;
 void move()
 {
 	// 16마리 물고기에 대해
-	for (int i = 1; i <= 16; i++) {
-		if (location[i] >= 0) {
+	for (int i = 1; i <= 16; i++) 
+	{
+		if (location[i] >= 0) 
+		{
 #if DEB
 			cout << "location: "<<location[i] << endl;
 #endif
 
-	int x = location[i] / 4, y = location[i] % 4;
-			// 8번 move
-		for (int j = 0; j < 8; j++) {
-			int cur_dir = (d[x][y] + j) % 8;
-			int nx = x + dir[cur_dir][0], ny = y + dir[cur_dir][1];
-			// 갈 수 없는 벽 만들기
-			if (nx < 0 || nx >= 4 || ny < 0 || ny >= 4)
-				continue;
-			// 상어 존재
-			if (g[nx][ny] == -1)
-				continue;
+			int x = location[i] / 4, y = location[i] % 4;
+				// 8번 move
+				for (int j = 0; j < 8; j++) 
+				{
+					int cur_dir = (d[x][y] + j) % 8;
+					int nx = x + dir[cur_dir][0], ny = y + dir[cur_dir][1];
+					
+					// 갈 수 없는 벽 만들기
+					if (nx < 0 || nx >= 4 || ny < 0 || ny >= 4)
+					continue;
+					
+					// 상어 존재
+					if (g[nx][ny] == -1)
+						continue;
 
-			// 빈 공간(Empty Space)
-			if (g[nx][ny] == 0) 
-			{
-				location[i] = 4 * nx + ny;
-				g[x][y] = 0;
-				d[x][y] = -1; //쓰레기값임. 어차피 의미 없음.
-				g[nx][ny] = i;
-				d[nx][ny] = cur_dir;
-				break;
+					// 빈 공간(Empty Space)
+					if (g[nx][ny] == 0) 
+					{
+						location[i] = 4 * nx + ny;
+						g[x][y] = 0;
+						d[x][y] = -1; //쓰레기값임. 어차피 의미 없음.
+						g[nx][ny] = i;
+						d[nx][ny] = cur_dir;
+						break;
+					}
+
+					// 다른 물고기가 있어 자리를 바꿈
+					if (g[nx][ny] > 0) 
+					{
+						location[i] = 4 * nx + ny;
+						location[g[nx][ny]] = 4 * x + y;
+						g[x][y] = g[nx][ny];
+						d[x][y] = d[nx][ny];
+						g[nx][ny] = i;
+						d[nx][ny] = cur_dir;
+						break;
+					}
+				}
 			}
+
+			
 }
 
 int main() {
